f09592e4668cef4f73754e672ee8fad38fe4f69c
tortolavivo23 <tortolavivo23@gmail.com>
2022-08-09 13:30:37 +0200

    Minor changes Signed-off-by: Francisco Tórtola <francisco.tortola@hablapps.com>

--- src/main/scala/syllogisms/figure1/Celarent.scala
+++ src/main/scala/syllogisms/figure1/Celarent.scala
@@ -8,14 +8,14 @@ object Celarent extends Syllogism {
   //No M is P
   type Major = (x: Entity) => x.M => Not[x.P]
 
-  //All S are P
+  //All S are M
   type Minor = (x: Entity) => x.S => x.M
 
   //No S is P
   type Conclusion = (x: Entity) => x.S => Not[x.P]
 
   def proof(major: Major, minor: Minor): Conclusion =
-    (x: Entity) => (s: x.S) => major(x) apply minor(x)(s)
+    (x: Entity) => (s: x.S) => (major(x) compose minor(x))(s)
 
 
 }
